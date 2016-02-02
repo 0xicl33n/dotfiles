5bcec5d3d3c6b80032796a362b64afc6a1218141
chris forst <0xicl33n@gmail.com>
2016-02-02 11:17:38 -0800

    Initial import

---
+++ .bashrc
@@ -0,0 +1,17 @@
+
+#Command aliases. Because lazy
+#debian 
+alias ag="apt-get"
+alias ap="aptitude"
+
+#arch linux
+alias p="sudo pacman"
+alias ya="yaourt"
+
+# Nvidia Jetson TK1 cuda stuff
+#export PATH=/usr/local/cuda-6.5/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+#export LD_LIBRARY_PATH=/usr/local/cuda-6.5/lib:
+#export __GL_PERFMON_MODE=1
+
+#ARM cross compiler 
+#export PATH=${PATH}:/root/arm-stuff/gcc-arm-linux-gnueabihf-4.7/bin
---
+++ config/fish/config.fish
@@ -0,0 +1,17 @@
+
+#Command aliases. Because lazy
+#debian 
+alias ag "apt-get"
+alias ap "aptitude"
+
+#arch linux
+alias p "sudo pacman"
+alias ya "yaourt"
+
+#Nvidia Jetson TK1 cuda stuff for fish
+#set -gx PATH /usr/local/cuda-6.5/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+#set -gx LD_LIBRARY_PATH /usr/local/cuda-6.5/lib:
+#set -gx  __GL_PERFMON_MODE 1
+
+#ARM cross compiler for fish (testing)
+#set -gx PATH ${PATH}:/root/arm-stuff/gcc-arm-linux-gnueabihf-4.7/bin
